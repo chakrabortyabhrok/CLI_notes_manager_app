4b546a181b9eca9a9e0d07fd29c11eea7b7221da
Abhrok Chakraborty <chakrabortyabhrok@gmail.com>
2026-02-19 13:26:00 +0530

    fixed persistence errors, added LOGIC LAYER

--- notes_manager.py
+++ notes_manager.py
@@ -2,7 +2,7 @@ import os
 import json
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-FILE_NAME =os.path.join(BASE_DIR, "notes.py")
+FILE_NAME =os.path.join(BASE_DIR, "notes.json")
 
 def load_notes():
     if not os.path.exists(FILE_NAME):
@@ -17,3 +17,40 @@ def load_notes():
 def save_notes(notes):
     with open (FILE_NAME, "w") as file:
         json.dump(notes, file, indent=4)
+
+def get_new_id(notes):
+    if not notes:
+        return 1
+    else:
+        return max(n["id"]for n in notes ) + 1
+    
+def add_notes(notes, notes_title, notes_description, notes_priority):
+    new_note = {
+        "id": get_new_id(notes),
+        "notes_info": {
+            "title": notes_title,
+            "description": notes_description,
+            "priority": notes_priority
+        }
+    }
+    notes.append(new_note)
+
+def delete_notes(notes, note_id):
+    for note in notes:
+        if note["id"] == note_id:
+            notes.remove(note)
+            return True
+    return False
+
+
+def show_ALL_priority(notes):
+    return [n["priority"] for n in notes]
+
+def show_HIGH_priority(notes):
+    return [n for n in notes if n["priority"] == "High"]
+
+def show_MEDIUM_priority(notes):
+    return [n for n in notes if n["priority"] == "Medium"]
+
+def show_LOW_priority(notes):
+    return [n for n in notes if n["priority"] == "Low"]
