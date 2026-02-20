1cb3b266443f22a4022d749d61c5e3650af97133
Abhrok Chakraborty <chakrabortyabhrok@gmail.com>
2026-02-20 14:19:36 +0530

    main() program complete, minor errors exists

--- notes_manager.py
+++ notes_manager.py
@@ -46,13 +46,11 @@ def get_notes_by_priority(notes, level):
     return[n for n in notes if n["notes_info"]["priority"] == level]
 
 MENU = """
-
 Add New Note           - a
 Delete Note            - d
 Show All Notes         - s
 Show Notes by priority - p
 Exit App               - e
-
 """
 def print_notes(notes):
     if not notes:
@@ -67,12 +65,48 @@ def print_notes(notes):
 def main():
     notes = load_notes()
     print("-- Welcome to the NOTES MANAGER ! --")
+
     while True:
         print(MENU)
         choice = input("Enter a choice: \n").lower().strip()
 
-        if choice == "s":
+        if choice == "a":
+            print("-- ADD NOTE --\n")
+            title = input("-- TITLE: (11 characters) --\n").lower().strip()
+            description = input("-- DECRIPTION: (25 characters) --\n").lower().strip()
+            priority = input("-- PRIORITY: --[High / Medium / Low]\n").lower().strip().capitalize()
+
+            if priority not in ["High", "Medium", "Low"]:
+                priority = "Medium"
+            print(priority)
+            add_notes(notes, title, description, priority)
+            save_notes(notes)
+            print("-- Notes Added --")
+
+        elif choice == "d":
+            print("-- DELETE NOTE --\n")
+            try:
+                note_id = int(input("Enter the note ID: \n"))
+                if delete_notes(notes, note_id):
+                    save_notes(notes)
+                    print("-- Note Deleted --")
+                else:
+                    print("-- ID not found --")
+
+            except ValueError:
+                print("-- Enter a valid ID --\n")
+            
+        elif choice == "s":
+            print("-- ALL NOTES --\n")
             print_notes(notes)
+
+        elif choice == "p":
+            print("-- SEARCH NOTES BY PRIORITY --")
+            level = input("Enter the priority level: --[High / Medium / Low]\n").lower().strip().capitalize()
+
+            if level not in ["High", "Medium", "Low"]:
+                print("-- no notes with aked priority --")
+            print_notes(get_notes_by_priority(notes, level))
 
         elif choice == "e":
             print("-- Goodbye --")
